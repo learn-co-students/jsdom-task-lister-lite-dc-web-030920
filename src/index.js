44d30625f370516a60dda818197cbd4f6f8a9177
github username <github email address>
2020-04-21 17:59:28 -0400

    Adds and Delete successfuly

--- src/index.js
+++ src/index.js
@@ -1,62 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
 
-// get the form 
-let formNode = document.getElementById("create-task-form");
-// add EventListener for the submit 
-formNode.addEventListener("submit", handelSubmit)
-
-// add onclick to show it is clicked
-
-
-
-
-
-
-
-
+let formNode = document.getElementById("create-task-form"); // get the form 
+formNode.addEventListener("submit", handelSubmit) // add EventListener for the submit 
+// document.getElementById('delete-button').onclick = deleteTask();
 
 });
-
-// add task discrption
-function addTask ( description){
-  // find the ul in the body 
-  //create li and appened to the ul 
-  let ulNode = document.querySelector("ul");
-  let liNode = document.createElement("li");
-  // add the user's input inside the li
-  liNode.innerText = description;
-  
-  //create button
-  let buttonNode = document.createElement("button");
-  buttonNode.id = "delete-button";
-  buttonNode.innerText = "X";
-  document.getElementById('delete-button').onclick = deleteTask();
- 
-  //append to li
-   liNode.appendChild(buttonNode);
-  //get task intput and put in inside the li 
-  ulNode.appendChild(liNode );
-  
-}
-
-function deleteTask(){
-  // get the task list
-  //remove the list
-  
-
-}
-
-// //
 function handelSubmit (event){
-  // use the prevent default method for submit
-  event.preventDefault();
+  event.preventDefault();// use the prevent default method for submit
+
   let disc = document.getElementById("new-task-description").value;
   addTask( disc);
 
   // after submit reset the form so that it doesn't have the user's task
   let formNode = document.getElementById("create-task-form");
   formNode.reset();
-
-
 }
+// ----------------------------------------------------------------------------------
+
+function addTask ( descrip){ // add task discrption
+  let ulNode = document.querySelector("ul");  // find the ul in the body 
+  let liNode = document.createElement("li"); //create li and appened to the ul 
+  liNode.innerText = descrip;// add the user's input inside the li
+
+  //create button for delete that is a child of li
+  let buttonNode = document.createElement("button");
+  buttonNode.innerText = "X";
+  liNode.appendChild(buttonNode);
+
+
+  // let selectNode = document.createElement("select");
+  // selectNode
+  
+
+  //get task intput and put in inside the li 
+  ulNode.appendChild(liNode );
+
+  buttonNode.addEventListener('click', function () { this.parentNode.remove() });
+
+  
+  
+}
+
+
+
+
+
+
